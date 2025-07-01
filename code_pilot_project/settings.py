fc95198b0f66fdca7341bf31e5431fbfbd9ebc23
NikhilChauhan0033 <chauhannikhil077@gmail.com>
2025-07-01 15:58:01 +0530

    final updated

--- code_pilot_project/settings.py
+++ code_pilot_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-sh_605r=sr!9$o2!v(m(f_(pj)6!*gx!)bwxtk^bwve^@-erj3
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # while your collect static and you style is gone make the debug True for applying styling we did the debug false for show the cutsmoe 404 page not found
-DEBUG = False
+DEBUG = True
 
 # while your collect static and you style is gone make the debug True for applying styling we did the debug false for show the cutsmoe 404 page not found
 
