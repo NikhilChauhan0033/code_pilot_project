764c7dbdeb37e1e2074c0ea165219f9c719ab2ad
NikhilChauhan0033 <chauhannikhil077@gmail.com>
2025-07-01 14:11:02 +0530

    Fix database config for Render

--- code_pilot_project/settings.py
+++ code_pilot_project/settings.py
@@ -88,9 +88,7 @@ WSGI_APPLICATION = 'code_pilot_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-       dj_database_url.config(conn_max_age=600)
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
